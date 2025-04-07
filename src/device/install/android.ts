added74884e03b62bcefc273174148e42f8684dc
Lukas Müller <WookieFPV@users.noreply.github.com>
2025-04-07 20:46:35 +0200

    chore: replace teen_process with zx

--- src/device/install/android.ts
+++ src/device/install/android.ts
@@ -1,29 +1,13 @@
-import { exec } from "teen_process";
+import { $ } from "zx";
 
 const adbPath = `${process.env.ANDROID_HOME}/platform-tools/adb`;
 
 export const installApk = async (apkPath: string) => {
-    const startTime = Date.now();
-    try {
-        const { code } = await exec(adbPath, ["install", "-r", "-d", apkPath]);
-        if (code !== 0) throw new Error(`Failed to install apk error code: ${code}`);
-        return { duration: Date.now() - startTime };
-    } catch (e) {
-        if (e instanceof Error && "stderr" in e) throw new Error(`${e.stderr}`);
-        throw new Error("unknown Error");
-    }
+    const { duration } = await $`${adbPath} install -r -d ${apkPath}`;
+    return { duration };
 };
 
 export const installSplitApk = async (apkFolder: string) => {
-    const startTime = Date.now();
-    try {
-        const { code } = await exec(adbPath, ["install-multiple", "-r", "-d", `${apkFolder}/*`], {
-            shell: true,
-        });
-        if (code !== 0) throw new Error(`Failed to install split apk error code: ${code}`);
-        return { duration: Date.now() - startTime };
-    } catch (e) {
-        if (e instanceof Error && "stderr" in e) throw new Error(`${e.stderr}`);
-        throw new Error("unknown Error");
-    }
+    const { duration } = await $`${adbPath} install-multiple -r -d ${apkFolder}/*`;
+    return { duration };
 };
