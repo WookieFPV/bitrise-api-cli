ec28e372b16991cbcae448558ebef4c1c48fdd0f
Lukas Müller <WookieFPV@users.noreply.github.com>
2025-04-09 22:26:02 +0200

    chore: inform user if the cli is not up to date

--- src/cli/app.ts
+++ src/cli/app.ts
@@ -15,5 +15,14 @@ export const app = buildApplication(root, {
     name,
     versionInfo: {
         currentVersion: version,
+        getLatestVersion: async () => {
+            const response = await fetch(`https://registry.npmjs.org/${name}/latest`);
+            if (!response.ok) throw new Error(`Failed to fetch the latest version of ${name}`);
+            const data = await response.json();
+            if (!data || typeof data !== "object" || !("version" in data) || typeof data.version !== "string")
+                throw new Error(`Failed to parse the latest version of ${name}`);
+            return data.version;
+        },
+        upgradeCommand: `npx ${name}@latest`,
     },
 });
